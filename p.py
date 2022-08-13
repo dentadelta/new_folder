045e9e615c9fda2828cfb15ed71c71af661789d7
delta <dentadelta123@gmail.com>
2022-08-14 06:44:10 +1000

    q

--- p.py
+++ p.py
@@ -16,3 +16,12 @@ model = T5ForConditionalGeneration.from_pretrained("google/t5-v1_1-base")
 from transformers import BloomTokenizerFast, BloomModel
 tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-1b3")
 model = BloomModel.from_pretrained("bigscience/bloom-1b3")
+
+from transformers import CLIPProcessor, CLIPModel
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
+model = GPT2LMHeadModel.from_pretrained("gpt2-large")
